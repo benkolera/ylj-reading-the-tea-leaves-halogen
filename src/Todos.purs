28a1d8296a7c28c1c5f0c476796d7ef3374cf131
Ben Kolera <ben.kolera@ephox.com>
2017-05-07 00:58:49 +1000

    Ex 3.2

--- src/Todos.purs
+++ src/Todos.purs
@@ -5,10 +5,14 @@ import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
+import DOM.Event.KeyboardEvent (code)
 import Data.Maybe (Maybe(..))
 import Debug.Trace (spy)
 
-data Query a = Toggle Int a | UpdateInput String a
+data Query a 
+  = Toggle Int a 
+  | UpdateInput String a
+  | NewTodo a
 
 type Todo = 
   { completed :: Boolean
@@ -19,6 +23,7 @@ type Todo =
 type State = 
   { todos :: Array Todo
   , editingStr :: String
+  , nextId :: Int
   }
 
 todos :: forall m. H.Component HH.HTML Query Unit Void m
@@ -34,6 +39,7 @@ todos =
   initialState :: State
   initialState = 
     { editingStr : ""
+    , nextId : 3
     , todos : 
       [ { completed : false, title : "Write Talk", todoId : 2 }
       , { completed : true, title : "Propose Talk", todoId : 1 }
@@ -60,10 +66,13 @@ todos =
         , HP.type_ HP.InputText 
         , HP.value state.editingStr
         , HE.onValueInput (HE.input UpdateInput)
+        , HE.onKeyPress (\e -> 
+          if code e == "Enter" then (Just $ H.action NewTodo) else Nothing
+          )
         ]
       , HH.ul [HP.class_ (H.ClassName "todo-list")]
         $ map renderTodo state.todos 
-        ]]
+      ]]
 
   eval :: Query ~> H.ComponentDSL State Query Void m
   eval = case _ of
@@ -78,6 +87,15 @@ todos =
         )
       pure next
     UpdateInput s next -> do
-      H.modify (_ { editingStr = spy s })
+      H.modify (_ { editingStr = s })
+      pure next
+    NewTodo next -> do
+      H.modify (\s -> s 
+        { editingStr = ""
+        , nextId = s.nextId + 1
+        , todos = 
+          [{ completed : false, title : s.editingStr, todoId : s.nextId}]
+          <> s.todos 
+        })
       pure next
 
